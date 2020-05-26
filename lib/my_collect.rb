422f30349fa4250a732fc524570e1feaf54b2ca6
Michael Takeuchi <miketaki@sbcglobal.net>
2020-05-26 23:54:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+  if block_given?
+    i = 0
+    result = []
+  
+    while i < array.length
+      result << (yield[i])
+      i += 1
+    end
+  else
+    "No block given"
+  end
+  result
+end
 
-
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
